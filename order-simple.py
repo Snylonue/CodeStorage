5abbbacbf0a01506af997b5e76d37bce80da138f
Snylonue <48825598+Snylonue@users.noreply.github.com>
2019-04-04 20:43:51 +0800

    Update order-simple.py
    注释

--- order-simple.py
+++ order-simple.py
@@ -1,14 +1,14 @@
 a=input()	
 a=a.split(',')
-for x1 in range(len(a)):   
-    a[x1]=float(a[x1])            #Input
-def correct(num):
-	for numx in range(len(num)-1):
+for x1 in range(len(a)):             #输入
+    a[x1]=float(a[x1])            
+def correct(num):                 
+	for numx in range(len(num)-1):   #判断顺序是否正确
 		if (num[numx]<num[numx+1] and numx==len(num)-1):
 			return True
 		if (num[numx]>num[numx+1]):
 			return False
-while (correct(a)==False):
+while (correct(a)==False):           #暴力排序
 	for x2 in range(len(a)-1):
 		if (a[x2]>a[x2+1]):
 			a[x2],a[x2+1]=a[x2+1],a[x2]
