3bb03020e6e54b16dff0f30fb9e8853d9f6674d1
Snylonue <48825598+Snylonue@users.noreply.github.com>
2019-04-05 17:45:06 +0800

    Update order-simple.py
    更换顺序

--- Order/order-simple.py
+++ Order/order-simple.py
@@ -1,13 +1,13 @@
-a=input()	
-a=a.split(',')
-for x1 in range(len(a)):             
-    a[x1]=float(a[x1])            
 def correct(num):                 
 	for numx in range(len(num)-1):
 		if (num[numx]>num[numx+1]):
 			return False
 		elif (numx==len(num)-2):
 			return True
+a=input()	
+a=a.split(',')
+for x1 in range(len(a)):
+    a[x1]=float(a[x1])
 while (correct(a)==False):           
 	for x2 in range(len(a)-1,0,-1):
 		max=0;
@@ -17,3 +17,4 @@ while (correct(a)==False):
 		a[max],a[x2]=a[x2],a[max]
 for out in a:
 	print(out,)
+	
