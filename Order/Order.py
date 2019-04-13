822d8d2b7d9b26ff3cefa1cca5053915023d4f66
Snylonue <48825598+Snylonue@users.noreply.github.com>
2019-04-13 20:57:21 +0800

    Update Order.py
    二分法排序完成
    修复无法排序问题

--- Order/Order.py
+++ Order/Order.py
@@ -12,16 +12,16 @@ def loca(funcl,insert):
 a=input()	
 a=a.split(',')
 for x1 in range(len(a)):
-    a[x1]=float(a[x1])
+	a[x1]=float(a[x1])
 for x2 in range(len(a)-1):
     if (a[x2]>a[x2+1]):
     	if (x2==0):
     		a[x2],a[x2+1]=a[x2+1],a[x2]
     	else:
-    		b=a[:x2]
+    		b=a[:x2+1]
     		t=a[x2+1]
     		point=loca(b,t)
-    		for x3 in range(x2+1,point+1,-1):
+    		for x3 in range(x2+1,point,-1):
     			a[x3]=a[x3-1]
     		a[point]=t
 for out in a:
